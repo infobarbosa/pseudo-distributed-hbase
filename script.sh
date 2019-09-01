860e5b4596d55ee994b7d44d5d2a82a8c1e3f690
Marcelo Barbosa <infobarbosa@yahoo.com.br>
2019-09-01 20:34:55 -0300

    Flexibilizando o projeto deixando no usuario do vagrant

--- script.sh
+++ script.sh
@@ -4,17 +4,15 @@
 sed -i "s/127.0.1.1/#127.0.1.1/" /etc/hosts
 echo "127.0.0.1	hbase.infobarbosa.github.com	hbase" >> /etc/hosts
 
-useradd -m hadoop
-echo "hadoop:hadoop" | chpasswd
 
-su hadoop -c "mkdir -p /home/hadoop/.ssh"
-su hadoop -c "ssh-keygen -t rsa -P '' -f /home/hadoop/.ssh/id_rsa"
-su hadoop -c "cat ~/.ssh/id_rsa.pub >> /home/hadoop/.ssh/authorized_keys"
-chmod 0600 /home/hadoop/.ssh/*
+su vagrant -c "mkdir -p /home/vagrant/.ssh"
+su vagrant -c "ssh-keygen -t rsa -P '' -f /home/vagrant/.ssh/id_rsa"
+su vagrant -c "cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys"
+chmod 0600 /home/vagrant/.ssh/*
 
 locale-gen en_US.UTF-8
 add-apt-repository ppa:openjdk-r/ppa
-apt-get -y update && apt-get install -y openjdk-8-jdk scala net-tools
+apt-get -y update && apt-get install -y openjdk-8-jdk net-tools
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre" >> /etc/profile
 echo "export PATH=${JAVA_HOME}/bin:${PATH}" >> /etc/profile
@@ -22,12 +20,12 @@ echo "export PATH=${JAVA_HOME}/bin:${PATH}" >> /etc/profile
 #wget -qO- http://ftp.unicamp.br/pub/apache/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz | tar xvz -C /opt/
 tar xzf hadoop-3.2.0.tar.gz
 mv hadoop-3.2.0 /opt/hadoop
-chown -R hadoop:hadoop /opt/hadoop
+chown vagrant:vagrant /opt/hadoop
 
 #wget -qO- https://www-us.apache.org/dist/hbase/stable/hbase-1.4.10-bin.tar.gz | tar xvz -C /opt/
 tar xzf hbase-1.4.10-bin.tar.gz
 mv hbase-1.4.10 /opt/hbase
-chown -R hadoop:hadoop /opt/hbase
+chown vagrant:vagrant /opt/hbase
 
 #variaveis de ambiente
 echo "export HADOOP_HOME=/opt/hadoop" >> /etc/profile
@@ -45,20 +43,13 @@ mv /home/vagrant/hadoop-env.sh /opt/hadoop/etc/hadoop/
 mv /home/vagrant/hbase-site.xml /opt/hbase/conf/
 mv /home/vagrant/hbase-env.sh /opt/hbase/conf/
 
-chown -R hadoop:hadoop /opt/hadoop/etc/hadoop/
-chown -R hadoop:hadoop /opt/hbase/conf
-
 #formatando o hdfs
-su hadoop -c "/opt/hadoop/bin/hdfs namenode -format"
+su vagrant -c "/opt/hadoop/bin/hdfs namenode -format"
 
 #configurando os systemd daemons
 mv hadoop.service /etc/systemd/system/
 mv yarn.service /etc/systemd/system/
 mv hbase.service /etc/systemd/system/
-
-chown hadoop:hadoop /etc/systemd/system/hadoop.service
-chown hadoop:hadoop /etc/systemd/system/yarn.service
-chown hadoop:hadoop /etc/systemd/system/hbase.service
 
 systemctl enable hadoop.service
 systemctl enable yarn.service
